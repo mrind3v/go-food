93b73632198e08a9e656fb45385fc2584033ee68
mrind3v <mrinmaydev@outlook.comm>
2025-10-21 16:36:55 +0000

    create routes related to users and also create user controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoutes.js";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/my/user", myUserRoute);
 
 const PORT = process.env.PORT || 7000;
 
